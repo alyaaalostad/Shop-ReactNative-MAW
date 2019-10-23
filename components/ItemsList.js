e9741540790c97cd1217c4d12f51c32620fc6c0c
Alyaa AlOstad <alyaaalostad@hotmail.com>
2019-10-23 16:22:55 +0300

    design done, but connect checkout to login and profile isn't

--- components/ItemsList.js
+++ components/ItemsList.js
@@ -6,7 +6,6 @@ import {
   List,
   Content,
   Button,
-  Text,
   Header,
   Left,
   Right,
@@ -14,12 +13,13 @@ import {
   Icon,
   Body
 } from "native-base";
-
+import { ImageBackground, Text, View, StyleSheet } from "react-native";
+import { login, signup, checkForExpiredToken, logout } from "../redux/actions";
 // Components
 import Loading from "./Loading";
 import ItemCard from "./ItemCard";
 import Searchbar from "./Searchbar";
-import { login, signup, checkForExpiredToken, logout } from "../redux/actions";
+import CartButton from "./CartButton";
 
 class ItemsList extends Component {
   render() {
@@ -27,11 +27,18 @@ class ItemsList extends Component {
       <ItemCard key={item.title} item={item} />
     ));
     return (
-      <Content>
-        <Searchbar />
-        <List>{this.props.loading ? <Loading /> : allItems}</List>
-        <></>
-      </Content>
+      <ImageBackground
+        source={{
+          uri:
+            "https://i.pinimg.com/474x/78/85/2c/78852cb7b283f3b465655c343f0ee92a.jpg"
+        }}
+        style={{ flex: 1, width: null, height: null }}
+      >
+        <Content>
+          <Searchbar />
+          <List>{this.props.loading ? <Loading /> : allItems}</List>
+        </Content>
+      </ImageBackground>
     );
   }
 }
@@ -49,7 +56,8 @@ const mapDispatchToProps = dispatch => {
 };
 ItemsList.navigationOptions = () => {
   return {
-    headerRight: <IconRegister />
+    headerLeft: <IconRegister />,
+    headerRight: <CartButton />
   };
 };
 
